cbe418ccf1585081690e2f91587b892ecd76f272
fourlena <elavlovic@gmail.com>
2022-06-28 18:31:35 +0300

    feat: update homework 5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
+import {PATH} from "./Pages";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.wrapper}>
+            <div className={s.links}>
+                <NavLink  to={PATH.PRE_JUNIOR} className={el => el.isActive ?  s.active : s.link} >pre-junior</NavLink>
+                <NavLink  to={PATH.JUNIOR} className={el => el.isActive ?  s.active : s.link}>junior</NavLink>
+                <NavLink  to={PATH.PRE_JUNIOR} className={el => el.isActive ?  s.active : s.link}>junior+</NavLink>
+            </div>
+            <button className={s.button}> click </button>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+            {/*<HW14/>*/}
+            {/*<HW15/>*/}
+            {/*<HW16/>*/}
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
